37b53c1693070ff991cd3de909045a1e763f2b48
Ethan Frank <edf32@case.edu>
2021-03-01 12:23:05 -0500

    Remove java gen from compile_protos.sh

--- protos/compile_protos.sh
+++ protos/compile_protos.sh
@@ -1,8 +1,5 @@
 # set -e
 
 # Generate python classes from protos
-python3 -m grpc_tools.protoc -I . --python_out=../python --grpc_python_out=../python hello_world.proto
-
-# Generate java classes from protos
-protoc -I . --java_out=. hello_world.proto
+python3 -m grpc_tools.protoc -I . --python_out=../python/protos/generated --grpc_python_out=../python/protos/generated hello_world.proto
 
